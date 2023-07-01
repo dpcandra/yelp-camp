a49032224a06a54296578f6681572bfbcb0a494a
dpcandra <dpcandra@gmail.com>
2023-07-01 21:41:06 +0800

    570. Adding custom Popups

--- models/campground.js
+++ models/campground.js
@@ -11,6 +11,8 @@ ImageSchema.virtual('thumbnail').get(function() {
    return this.url.replace('/upload', '/upload/w_200');
 })
 
+const opts = { toJSON: { virtuals:true } };
+
 const CampgroundSchema = new Schema({
     title: {type:String},
     images: [ImageSchema],
@@ -38,7 +40,12 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
-});
+},opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function() {
+    return `<strong><a href="/campgrounds/${this.id}">${this.title}</a></strong>
+            <p>${this.description.substring(0,30)}...</p>`;
+ })
 
 CampgroundSchema.post('findOneAndDelete', async function (campground){
     if(campground.reviews.length){
--- seed/index.js
+++ seed/index.js
@@ -17,19 +17,19 @@ const sample = (array) =>{
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i=0;i<50; i++){
-        const random100 = Math.floor(Math.random()*1000);
+    for(let i=0;i<200; i++){
+        const random1000 = Math.floor(Math.random()*1000);
         const price = Math.floor(Math.random() * 20) +10 ;
         const camp = new Campground({
             author : '64987390af6d16da71948556',
-            location: `${cities[random100].city}, ${cities[random100].state}`,
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             // image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti nobis quidem libero. Ut, a consectetur dignissimos expedita, alias beatae nemo eum reprehenderit dolor libero, architecto modi neque blanditiis minus ipsum.',
             price : price,
             geometry: { 
                 type: 'Point', 
-                coordinates: [ -113.1331, 47.0202 ] 
+                coordinates: [ cities[random1000].longitude, cities[random1000].latitude ] 
             },
             images:  [
                 {
